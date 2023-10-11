ce3a04fd0e39602ade9bf9578bb0cb51c20dfcc9
Tayeb <tayeb.ezzaoui4@gmail.com>
2023-10-11 14:58:08 +0200

    <feat> ajout de la page de prestation

--- AP1-GP2-PhoTographe/var/cache/dev/twig/fb/fb7fc7d905a6024f7635470343cda798.php
+++ AP1-GP2-PhoTographe/var/cache/dev/twig/fb/fb7fc7d905a6024f7635470343cda798.php
@@ -24,13 +24,17 @@ class __TwigTemplate_049c63243bf8b3538f8ef2a10ef001ce extends Template
 
         $this->source = $this->getSourceContext();
 
-        $this->parent = false;
-
         $this->blocks = [
             'title' => [$this, 'block_title'],
-            'stylesheet' => [$this, 'block_stylesheet'],
+            'stylesheets' => [$this, 'block_stylesheets'],
             'body' => [$this, 'block_body'],
         ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "base.html.twig";
     }
 
     protected function doDisplay(array $context, array $blocks = [])
@@ -39,54 +43,32 @@ class __TwigTemplate_049c63243bf8b3538f8ef2a10ef001ce extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "c_principal/index.html.twig"));
 
-        // line 1
-        echo "<!DOCTYPE html>
-<html>
-<head>
-    <meta charset=\"utf-8\">
-";
-        // line 5
-        $this->displayBlock('title', $context, $blocks);
-        // line 6
-        echo "
-     
-";
-        // line 8
-        $this->displayBlock('stylesheet', $context, $blocks);
-        // line 9
-        echo "
-
-</head>
-
-";
-        // line 13
-        $this->displayBlock('body', $context, $blocks);
-        // line 34
-        echo "</html>
-";
+        $this->parent = $this->loadTemplate("base.html.twig", "c_principal/index.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
 
     }
 
-    // line 5
+    // line 3
     public function block_title($context, array $blocks = [])
     {
         $macros = $this->macros;
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
 
+        echo " <title>Accueil </title>";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
 
     }
 
-    // line 8
-    public function block_stylesheet($context, array $blocks = [])
+    // line 6
+    public function block_stylesheets($context, array $blocks = [])
     {
         $macros = $this->macros;
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheet"));
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
 
         echo "<link rel=\"stylesheet\" href=\"assets/css/style_accueil.css\"> ";
         
@@ -94,32 +76,37 @@ class __TwigTemplate_049c63243bf8b3538f8ef2a10ef001ce extends Template
 
     }
 
-    // line 13
+    // line 10
     public function block_body($context, array $blocks = [])
     {
         $macros = $this->macros;
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
 
-        // line 14
+        // line 11
         echo "
-<body class=\"accueil-login\">
-    <nav>
+<body class=\"accueil-login grid-content\">
+    <nav class=\"grid-nav\">
         <ul class=\"nav-items\">
             <li><a href=\"index.php\">Acceuil</a></li>
             <li><a href=\"inscription.php\">Présentations</a></li>
-            <li><a href=\"connexion.php\">Préstations</a></li>
+            <li><a href=\"connexion.php\">Prestations</a></li>
             <li><a href=\"profil.php\">Avis</a></li>
             <li><a href=\"admin.php\">Contact</a></li>
         </ul>
     </nav>
-    <h1 class=\"titile-acueille\">AlexCaptures</h1>
+    <h1 class=\"titile-acueille grid-title\">AlexCaptures</h1>
 
-    <section class=\"login-choice\">
+    <section class=\"login-choice grid-login\">
         <button class=\"btn-login\"> <a href=\"/connexion\">Se connecter </a></button>
         <p class=\"txt-login\">ou</p>
         <button class=\"btn-login\"><a href=\"/inscription\">Créer un compte </a></button>
     </section>
+
+    <section class=\"grid-presentation\">
+    Je suis Alex, un photographe passionné qui transforme vos moments spéciaux en souvenirs durables. Mon expertise et ma créativité se combinent pour vous offrir des images exceptionnelles.
+    </section>
+        
 </body>
 ";
         
@@ -132,47 +119,53 @@ class __TwigTemplate_049c63243bf8b3538f8ef2a10ef001ce extends Template
         return "c_principal/index.html.twig";
     }
 
+    public function isTraitable()
+    {
+        return false;
+    }
+
     public function getDebugInfo()
     {
-        return array (  105 => 14,  98 => 13,  85 => 8,  73 => 5,  65 => 34,  63 => 13,  57 => 9,  55 => 8,  51 => 6,  49 => 5,  43 => 1,);
+        return array (  87 => 11,  80 => 10,  67 => 6,  54 => 3,  37 => 1,);
     }
 
     public function getSourceContext()
     {
-        return new Source("<!DOCTYPE html>
-<html>
-<head>
-    <meta charset=\"utf-8\">
-{% block title %}{% endblock %}
+        return new Source("{% extends 'base.html.twig' %}
+
+{% block title %} <title>Accueil </title>{% endblock %}
 
      
-{% block stylesheet %}<link rel=\"stylesheet\" href=\"assets/css/style_accueil.css\"> {% endblock %}
+{% block stylesheets %}<link rel=\"stylesheet\" href=\"assets/css/style_accueil.css\"> {% endblock %}
 
 
-</head>
 
 {% block body %}
 
-<body class=\"accueil-login\">
-    <nav>
+<body class=\"accueil-login grid-content\">
+    <nav class=\"grid-nav\">
         <ul class=\"nav-items\">
             <li><a href=\"index.php\">Acceuil</a></li>
             <li><a href=\"inscription.php\">Présentations</a></li>
-            <li><a href=\"connexion.php\">Préstations</a></li>
+            <li><a href=\"connexion.php\">Prestations</a></li>
             <li><a href=\"profil.php\">Avis</a></li>
             <li><a href=\"admin.php\">Contact</a></li>
         </ul>
     </nav>
-    <h1 class=\"titile-acueille\">AlexCaptures</h1>
+    <h1 class=\"titile-acueille grid-title\">AlexCaptures</h1>
 
-    <section class=\"login-choice\">
+    <section class=\"login-choice grid-login\">
         <button class=\"btn-login\"> <a href=\"/connexion\">Se connecter </a></button>
         <p class=\"txt-login\">ou</p>
         <button class=\"btn-login\"><a href=\"/inscription\">Créer un compte </a></button>
     </section>
+
+    <section class=\"grid-presentation\">
+    Je suis Alex, un photographe passionné qui transforme vos moments spéciaux en souvenirs durables. Mon expertise et ma créativité se combinent pour vous offrir des images exceptionnelles.
+    </section>
+        
 </body>
 {% endblock %}
-</html>
 ", "c_principal/index.html.twig", "C:\\Users\\ezzaouit\\Desktop\\Atteliers\\AP1-GP2-Photographe\\AP1-GP2-PhoTographe\\templates\\c_principal\\index.html.twig");
     }
 }
